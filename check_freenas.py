60877191b210662e4efdabd4cc6732f92d0a1fab
Patrick Byrne <patricknbyrne@gmail.com>
2018-12-08 21:44:44 -0500

    Add verbose and SSL ignore options. Add additional handling for SSL errors.

--- check_freenas.py
+++ check_freenas.py
@@ -12,7 +12,7 @@
 #   Add storage utilization check
 # ------------------------------------------------------------------
 
-__version__ = "1.0.1"
+__version__ = "1.2.0"
 
 # ------------------------------------------------------------------
 
@@ -23,12 +23,15 @@ import requests
 
 class FreenasAPI(object):
 
-    def __init__(self, hostname, user, secret, timeout=5):
+    def __init__(self, hostname, user, secret, verbose, verify, timeout=5):
+        requests.packages.urllib3.disable_warnings()
         self._hostname = hostname
         self._user = user
         self._secret = secret
         self._url = 'http://%s/api/v1.0' % hostname
         self.timeout = timeout
+        self.verify = verify
+        self.verbose = verbose
 
     # Function Sends a request to the Freenas API
     def _request(self, resource):
@@ -38,6 +41,7 @@ class FreenasAPI(object):
                 '%s/%s/' % (self._url, resource),
                 auth=(self._user, self._secret),
                 timeout=self.timeout,
+                verify=self.verify
             )
             r.raise_for_status()
         except requests.exceptions.HTTPError as error:
@@ -46,8 +50,13 @@ class FreenasAPI(object):
             output_results((3, "Error: Too many redirects", None))
         except requests.exceptions.Timeout:
             output_results((3, "Error: Timeout exceeded", None))
+        #except requests.exceptions.SSLError:
+        #    output_results((3, "Error: Unable to Verify SSL Certificate", None))
         except requests.exceptions.RequestException:
-            output_results((3, "Error: Unknown error", None))
+            if not self.verbose:
+                output_results((3, "Error: Unknown error", None))
+            else:
+                raise
 
         # If data returned is not JSON formatted, fail to output parser
         try:
@@ -92,6 +101,16 @@ class FreenasAPI(object):
         # If all the disks are online, return "OK"
         return (0, "All disks are online", None)
 
+    # Check the alert status of the device
+    def check_alerts(self):
+        # Get alert status on the device
+        alert_status = self._request('system/alert')
+        print (alert_status)
+        sys.exit (0)
+
+        # Return OK if all volumes are healthy
+        #return (0, "All volumes are healthy", None)
+
 # Format results for Nagios processing
 def output_results(*exitstatus):
     # Map our incoming variables
@@ -121,26 +140,39 @@ def output_results(*exitstatus):
 def main():
     # Setup arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--hostname', required=True, type=str)
-    parser.add_argument('-u', '--user',     required=True, type=str)
-    parser.add_argument('-p', '--passwd',   required=True, type=str)
-    parser.add_argument('-t', '--timeout',  required=False, type=int)
-    parser.add_argument('-c', '--check',    required=True, type=str,
-                                            choices=[   'disks',
-                                                        'volumes',
-                                                        ])
+    parser.add_argument('-H', '--hostname',     required=True, type=str)
+    parser.add_argument('-u', '--user',         required=True, type=str)
+    parser.add_argument('-p', '--passwd',       required=True, type=str)
+    parser.add_argument('-t', '--timeout',      required=False, type=int)
+    parser.add_argument('-v', '--verbose',      required=False, default=False, 
+                                                action='store_true')
+    parser.add_argument('-i', '--ignorecerts',  required=False, default=True, 
+                                                action='store_false')
+    parser.add_argument('-c', '--check',        required=True, type=str,
+                                                choices=[   'disks',
+                                                            'volumes',
+                                                            'alerts',
+                                                            ])
 
     # Parse arguments
     args = parser.parse_args(sys.argv[1:])
 
     # Setup Freenas API request object 
-    request = FreenasAPI(args.hostname, args.user, args.passwd, args.timeout)
+    request = FreenasAPI(   args.hostname, 
+                            args.user, 
+                            args.passwd, 
+                            args.verbose, 
+                            args.ignorecerts,
+                            args.timeout 
+                            )
 
     # Parse check type and send request to API
     if args.check == "disks":
         output_results(request.check_disks())
     elif args.check == "volumes":
         output_results(request.check_volumes())
+    elif args.check == "alerts":
+        output_results(request.check_alerts())
 
 if __name__ == '__main__':
     main()
